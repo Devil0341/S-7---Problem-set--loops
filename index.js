6c21bb530dc5fd5a191337a1161d9d9e41965911
Greg <gregpetropoulos@yahoo.com>
2020-12-18 11:26:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Write a program that prints the numbers 1 - 50. If the number is even print the word 'EVEN!' next to it. If the number is odd print the word 'ODD!' next to it.
+
+
+for (i=0; i <= 50; i++){
+  if (i === 0 || i % 2 == 0){
+    console.log(i+ ' ' +'Even')
+  }
+  else {
+    console.log(i + ' ' + 'Odd');
+  }
+}
